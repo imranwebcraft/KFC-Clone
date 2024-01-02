fa2df93c516d4153cb13178676db1ce7e41e1e5c
imranwebcraft <imranhossainsakil22@gmail.com>
2024-01-02 13:31:35 +0600

    CTA section added

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,9 +1,29 @@
+import { FaGooglePlay } from 'react-icons/fa';
+import { FaApple } from 'react-icons/fa';
+
 const CTA = () => {
 	return (
-		<div className=" bg-white py-32">
-			<h3>CTA</h3>
+		<div className="bg-white/85 pt-[106px] pb-[81px] border-b border-gray-100 px-6">
+			<div className=" max-w-7xl mx-auto flex flex-col gap-5">
+				<h3 className=" text-xl font-medium text-black">
+					Even easier in the app
+				</h3>
+				<div className=" flex gap-5">
+					{/* ---App Store */}
+					<div className="flex items-center gap-3 px-3 py-[6px] rounded-[7px] border border-gray-300 text-gray-500">
+						<FaApple size={22} />
+						<p className=" text-sm">App Store</p>
+					</div>
+					{/* ----Play Store---- */}
+					<div className=" flex items-center gap-3 px-3 py-[6px] rounded-[7px] border border-gray-300 text-gray-500">
+						<FaGooglePlay size={18} />
+						<p className=" text-sm">PLAY STORE</p>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
 
+// eslint-disable-next-line react-refresh/only-export-components
 export default CTA;
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,14 +2,14 @@ import emptyBag from '../assets/Empty.svg';
 
 const Cart = () => {
 	return (
-		<div className="h-screen p-5">
-			<div className=" h-full flex flex-col justify-between">
-				<div className=" flex justify-start items-center py-5">
+		<div className="h-screen p-5 max-w-[320px]">
+			<div className="h-full flex flex-col justify-between">
+				<div className=" flex justify-start items-center py-2">
 					<h1 className=" text-2xl font-semibold">Cart</h1>
 				</div>
 				<div className=" flex flex-col justify-center items-center">
 					<img src={emptyBag} alt="emptyCart" className=" w-[50%] h-[50%]" />
-					<p className="text-2xl text-center font-semibold">
+					<p className="text-2xl text-center font-semibold mt-2">
 						Your cart is currently empty
 					</p>
 				</div>
