95b0756257edf1e254b88f9d78c462c88f575293
imranwebcraft <imranhossainsakil22@gmail.com>
2024-01-02 15:28:47 +0600

    free delivery section added

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,9 +1,12 @@
 import { MdOutlineStar } from 'react-icons/md';
 import { IoMdInformationCircleOutline } from 'react-icons/io';
+import dicountBg from '../assets/discountbg.svg';
+import { FaGift } from 'react-icons/fa6';
 
 const Content = () => {
 	return (
 		<section>
+			{/* Image */}
 			<section className=" relative bg-custom-image w-[100%] h-[360px] rounded-[36px]">
 				<div className=" w-full h-full flex flex-col justify-center items-start absolute left-10 top-16 space-y-4">
 					<h1 className=" text-5xl font-semibold text-white z-[99]">KFC</h1>
@@ -19,6 +22,15 @@ const Content = () => {
 							<IoMdInformationCircleOutline size={35} />
 						</div>
 					</div>
+				</div>
+			</section>
+
+			{/* Free Delivery */}
+			<section className="py-12">
+				<div className="relative flex items-center gap-2 bg-[#72aa52] bg-opacity-20 px-3 py-4 rounded-[16px]">
+					<img src={dicountBg} alt="discountBGImage" className=" relative" />
+					<p className=" text-[#72aa52]">Free delivery – off any order</p>
+					<FaGift size={20} className=" text-white absolute left-[26px]" />
 				</div>
 			</section>
 		</section>
